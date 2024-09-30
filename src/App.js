e4bb113b7df6ce6ecad589883209d61290f14fba
Nou7en <tomh.tc89@gmail.com>
2024-09-30 11:58:37 -0500

    Final

--- src/App.js
+++ src/App.js
@@ -9,17 +9,36 @@ const data = [
   { id: 2, plato: "Pollo a la Plancha", tipo: "China" },
   { id: 3, plato: "Tallarines Saltados", tipo: "China" },
   { id: 4, plato: "Sopa Wantán", tipo: "China" },
+  { id: 5, plato: "Apanado", tipo: "China" },
+  { id: 6, plato: "Mixto", tipo: "China" },
+  { id: 7, plato: "Lomo Saltado", tipo: "China" },
+  { id: 8, plato: "Gyosas", tipo: "Coreana" },
+  { id: 9, plato: "Sushi", tipo: "Janponess" },
+  { id: 10, plato: "Dim Sum", tipo: "Coreana" },
+  { id: 11, plato: "Salteado de Verduras", tipo: "Taiwan" },
 ];
 
 class App extends React.Component {
   state = {
     data: data,
+    modalActualizar: false,
     modalInsertar: false,
     form: {
       id: "",
       plato: "",
       tipo: "",
     },
+  };
+
+  mostrarModalActualizar = (dato) => {
+    this.setState({
+      form: dato,
+      modalActualizar: true,
+    });
+  };
+
+  cerrarModalActualizar = () => {
+    this.setState({ modalActualizar: false });
   };
 
   mostrarModalInsertar = () => {
@@ -32,20 +51,40 @@ class App extends React.Component {
     this.setState({ modalInsertar: false });
   };
 
+  editar = (dato) => {
+    var contador = 0;
+    var arreglo = this.state.data;
+    arreglo.map((registro) => {
+      if (dato.id === registro.id) {
+        arreglo[contador].plato = dato.plato;
+        arreglo[contador].tipo = dato.tipo;
+      }
+      contador++;
+    });
+    this.setState({ data: arreglo, modalActualizar: false });
+  };
+
+  eliminar = (dato) => {
+    var opcion = window.confirm("¿Estás seguro que deseas eliminar el plato " + dato.plato + "?");
+    if (opcion === true) {
+      var contador = 0;
+      var arreglo = this.state.data;
+      arreglo.map((registro) => {
+        if (dato.id === registro.id) {
+          arreglo.splice(contador, 1);
+        }
+        contador++;
+      });
+      this.setState({ data: arreglo, modalActualizar: false });
+    }
+  };
+
   insertar = () => {
     var valorNuevo = { ...this.state.form };
     valorNuevo.id = this.state.data.length + 1;
     var lista = this.state.data;
     lista.push(valorNuevo);
     this.setState({ modalInsertar: false, data: lista });
-  };
-
-  eliminar = (dato) => {
-    var opcion = window.confirm("¿Estás seguro que deseas eliminar el plato " + dato.plato + "?");
-    if (opcion === true) {
-      var arreglo = this.state.data.filter(item => item.id !== dato.id);
-      this.setState({ data: arreglo });
-    }
   };
 
   handleChange = (e) => {
@@ -59,33 +98,94 @@ class App extends React.Component {
 
   render() {
     return (
-      <Container>
-        <br />
-        <Button color="success" onClick={() => this.mostrarModalInsertar()}>Crear</Button>
-        <br />
-        <br />
-        <Table>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Plato</th>
-              <th>Tipo</th>
-              <th>Acción</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.data.map((dato) => (
-              <tr key={dato.id}>
-                <td>{dato.id}</td>
-                <td>{dato.plato}</td>
-                <td>{dato.tipo}</td>
-                <td>
-                  <Button color="danger" onClick={() => this.eliminar(dato)}>Eliminar</Button>
-                </td>
+      <>
+        <Container>
+          <br />
+          <Button color="success" onClick={() => this.mostrarModalInsertar()}>Crear</Button>
+          <br />
+          <br />
+          <Table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Plato</th>
+                <th>Tipo</th>
+                <th>Acción</th>
               </tr>
-            ))}
-          </tbody>
-        </Table>
+            </thead>
+            <tbody>
+              {this.state.data.map((dato) => (
+                <tr key={dato.id}>
+                  <td>{dato.id}</td>
+                  <td>{dato.plato}</td>
+                  <td>{dato.tipo}</td>
+                  <td>
+                    <Button
+                      color="primary"
+                      onClick={() => this.mostrarModalActualizar(dato)}
+                    >
+                      Editar
+                    </Button>{" "}
+                    <Button color="danger" onClick={() => this.eliminar(dato)}>Eliminar</Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Container>
+
+        <Modal isOpen={this.state.modalActualizar}>
+          <ModalHeader>
+            <div><h3>Editar Plato</h3></div>
+          </ModalHeader>
+
+          <ModalBody>
+            <FormGroup>
+              <label>Id:</label>
+              <input
+                className="form-control"
+                readOnly
+                type="text"
+                value={this.state.form.id}
+              />
+            </FormGroup>
+            <FormGroup>
+              <label>Plato:</label>
+              <input
+                className="form-control"
+                name="plato"
+                type="text"
+                onChange={this.handleChange}
+                value={this.state.form.plato}
+              />
+            </FormGroup>
+            <FormGroup>
+              <label>Tipo:</label>
+              <input
+                className="form-control"
+                name="tipo"
+                type="text"
+                onChange={this.handleChange}
+                value={this.state.form.tipo}
+              />
+            </FormGroup>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button
+              color="primary"
+              onClick={() => this.editar(this.state.form)}
+            >
+              Confirmar
+            </Button>
+            <Button
+              color="danger"
+              onClick={() => this.cerrarModalActualizar()}
+            >
+              Cancelar
+            </Button>
+          </ModalFooter>
+        </Modal>
 
         <Modal isOpen={this.state.modalInsertar}>
           <ModalHeader>
@@ -93,6 +193,15 @@ class App extends React.Component {
           </ModalHeader>
 
           <ModalBody>
+            <FormGroup>
+              <label>Id:</label>
+              <input
+                className="form-control"
+                readOnly
+                type="text"
+                value={this.state.data.length + 1}
+              />
+            </FormGroup>
             <FormGroup>
               <label>Plato:</label>
               <input
@@ -114,13 +223,26 @@ class App extends React.Component {
           </ModalBody>
 
           <ModalFooter>
-            <Button color="primary" onClick={() => this.insertar()}>Insertar</Button>
-            <Button className="btn btn-danger" onClick={() => this.cerrarModalInsertar()}>Cancelar</Button>
+            <Button
+              color="primary"
+              onClick={() => this.insertar()}
+            >
+              Insertar
+            </Button>
+            <Button
+              className="btn btn-danger"
+              onClick={() => this.cerrarModalInsertar()}
+            >
+              Cancelar
+            </Button>
           </ModalFooter>
         </Modal>
-      </Container>
+      </>
     );
   }
 }
+
+
+
 
 export default App;
